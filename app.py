89bd881e98d663fb661f3d79e861a29065f2b9af
kimdelabr <94327386+kimdelabr@users.noreply.github.com>
2023-08-15 06:07:13 +0000

    Update app.py

--- app.py
+++ app.py
@@ -15,8 +15,8 @@ def hello_world():
     img2 = "static/cat_or_dog_2.jpg"
     img3 = "static/cat_or_dog_3.jpg"
 
-    #model_final = load_model("mobNet_model_tf.tf/saved_model.pb")
-    model_final = tf.saved_model.load("mobNet_model_tf.tf")
+    model_final = load_model("mobNet_model_tf.tf/saved_model.pb", custom_objects=None)
+    #model_final = tf.saved_model.load("mobNet_model_tf.tf")
 
 
     img_file = img3
