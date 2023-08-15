c7f81a57aa470632796d13aa2da3c8eb70db9e7e
kimdelabr <kimaarnaud1@gmail.com>
2023-08-15 02:51:51 +0000

    Modification du fichier app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request
 import numpy as np
+import tensorflow as tf
+
 #from tensorflow.keras.preprocessing import image
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
@@ -16,20 +18,27 @@ def hello_world():
     img2 = "static/cat_or_dog_2.jpg"
     img3 = "static/cat_or_dog_3.jpg"
 
-    model_final = load_model("model_cnn.h5")
+    file = "mobNet_model_tf.tf"
+    # Load the entire model (architecture and weights)
+    loaded_model = tf.keras.models.load_model(file)
+    
 
-    img_file = img3
-    #test_image = load_img(img_file, target_size = (64, 64))
-    test_image = Image.open(img_file).resize((64, 64))
+    img_file = img1
 
-    test_image = img_to_array(test_image)
+    test_image = load_img(img_file, target_size = (224, 224))
+    test_image = np.array(test_image)
     test_image = np.expand_dims(test_image, axis = 0)
-    result = model_final.predict(test_image)
-    #training_set.class_indices
-    if result[0][0] == 1:
-        return 'chien'
+
+    result = loaded_model.predict(test_image)
+
+    if result[0][0]  <= 0.85:
+        classif = 'chien'
     else:
-        return 'chat'
+        classif = 'chat'
+
+    print(classif)
+
+
 
 app.debug = True
 
